efeae8ca192890d47eb2ede140f7ae7698a6f2c4
charafbel <114723961+charafbel@users.noreply.github.com>
2022-10-07 08:45:44 +0200

    update village 1

--- personnages/Village.java
+++ personnages/Village.java
@@ -3,21 +3,34 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois gaulois;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 	
-	public Village(String nom, Chef chef) {
+	
+	
+	public Village(String nom, Chef chef, Gaulois[] villageois, int nbVillageoisMaximum, int nbVillageois, Gaulois gaulois) {
 		super();
 		this.nom = nom;
 		this.chef = chef;
+		this.villageois = villageois;
+		this.nbVillageois = nbVillageois;
+		this.gaulois = gaulois;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
 	
 	
-	public void setChef(Chef chef) {
+	public void setChef(Chef chef) 
 		this.chef = chef;
 	}
-	
 
+
+	public void ajouterHabitant(Gaulois gaulois, Gaulois[] villageois) {
+		villageois.add(gaulois);
+		nbVillageois++;
+		
+	}
 }
